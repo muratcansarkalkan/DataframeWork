34b9bc086d18595874ffbd4286b713df47b0d045
Muratcan Sarkalkan <40072414+muratcansarkalkan@users.noreply.github.com>
2022-02-23 16:18:51 +0300

    Add files via upload

---
+++ BasicDataframe.py
@@ -0,0 +1,7 @@
+# burada dataframele çok şey denicem
+import pandas as pd
+
+d = {'Size': [2104, 1416, 1534, 852], 'No of bedrooms': [5, 3, 3, 2]}
+df = pd.DataFrame(data=d)
+
+df
---
+++ DataGroupTableset.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Sets a table for mean, median, mode... etc
+
+d0 = {"Data": [15, 4, 3, 8, 15, 22, 7, 9, 2, 3, 3, 12, 6]}
+df = pd.DataFrame(d0)
+
+y = df['Data'].mean()
+s = df['Data'].median()
+q = df['Data'].mode()
+a = df['Data'].quantile(.1)
+b = df['Data'].max() - df['Data'].min()
+vari = df['Data'].var()
+dev = df['Data'].std()
+
+d = {'Info': ["Mean", "Median", "Mode", "Quantile", "Range", "Variance", "StDev"], 'Values': [y, s, q, a, b, vari, dev]}
+dx = pd.DataFrame(data=d)
